6ddc431bad418bc248563df8ea212d6e13ea85ba
pandroe <daffapandu2304@gmail.com>
2022-11-27 21:13:53 +0700

    mengganti nama folder register menjadi register_screen

---
+++ payoll/lib/views/register_screen/widgets/register_button.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class RegisterButton extends StatelessWidget {
+  const RegisterButton({super.key, required this.onPressed});
+  final VoidCallback onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.all(12.0),
+            backgroundColor: Color(0xFF396EB0),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(8))),
+        onPressed: onPressed,
+        child: Text(
+          'Daftar',
+          style: TextStyle(fontWeight: FontWeight.w500),
+        ));
+  }
+}
---
+++ payoll/lib/views/register_screen/widgets/register_button_google.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class RegisterGoogle extends StatelessWidget {
+  const RegisterGoogle({super.key, required this.onPressed});
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.all(12.0),
+            side: BorderSide(color: Color(0xFFCFCFCF)),
+            backgroundColor: Colors.white,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(8))),
+        onPressed: () {},
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+                height: 28.0,
+                width: 28.0,
+                child: Image.asset(
+                  'assets/images/google.png',
+                )),
+            Text(
+              ' GOOGLE',
+              style: TextStyle(
+                  color: Colors.black, fontWeight: FontWeight.w500),
+            )
+          ],
+        ));
+  }
+}
