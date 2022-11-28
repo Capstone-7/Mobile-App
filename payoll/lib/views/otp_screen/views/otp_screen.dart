6db65c4a747256b9fabe28baf4487527048ddc70
pandroe <daffapandu2304@gmail.com>
2022-11-28 17:41:55 +0700

    membuat tampilan otp

---
+++ payoll/lib/views/otp_screen/views/otp_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:payoll/views/otp_screen/widgets/otp_input.dart';
+import '../../../utils/constant.dart';
+import '../../login_screen/views/login_screen.dart';
+import '../widgets/otp_app_bar.dart';
+import '../widgets/otp_send_button.dart';
+
+class OtpScreen extends StatefulWidget {
+  static const String routeName = 'otp-screen';
+  const OtpScreen({super.key});
+
+  @override
+  State<OtpScreen> createState() => _OtpScreenState();
+}
+
+class _OtpScreenState extends State<OtpScreen> {
+  final GlobalKey formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: otpAppBar(context),
+      resizeToAvoidBottomInset: false,
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text('Kode OTP telah kami kirimkan ke F*********@gmail.com',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: Constant.fontSemiRegular)),
+            SizedBox(
+              height: size.height * 0.036,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                OtpInput(),
+                OtpInput(),
+                OtpInput(),
+                OtpInput(),
+                OtpInput(),
+                OtpInput(),
+              ],
+            ),
+            SizedBox(
+                        height: size.height * 0.020,
+                      ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Tidak mendapat kode?'),
+                TextButton(onPressed: (){}, child: Text('Kirim Ulang'))
+              ],
+            ),
+                      OtpVerificationButton(
+                        onPressed: () {},
+                      ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
---
+++ payoll/lib/views/otp_screen/widgets/otp_app_bar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:payoll/utils/constant.dart';
+
+PreferredSizeWidget otpAppBar (BuildContext context){
+  String routeName = 'forgot-password-app-bar';
+  return AppBar(
+    backgroundColor: Colors.transparent,
+    elevation: 0,
+    leading: IconButton(onPressed: () => Navigator.pop(context), icon: Icon(FontAwesomeIcons.chevronLeft, color: Colors.black),),
+    title: Row(
+      children: [
+        Text('Verifikasi Kode OTP',style: TextStyle(color: Colors.black, fontSize: Constant.fontBig, fontWeight: FontWeight.bold), )
+      ],
+    ),
+  );
+}
