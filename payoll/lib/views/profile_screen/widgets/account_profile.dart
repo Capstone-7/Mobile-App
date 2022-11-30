b7168ea8f6289a2edfc4b8441ee0b40648a653e5
pandroe <daffapandu2304@gmail.com>
2022-11-30 21:10:58 +0700

    membuat tampilan profile screen beserta widgetnya

---
+++ payoll/lib/views/profile_screen/widgets/account_profile.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import '../../../utils/constant.dart';
+
+class AccountProfile extends StatelessWidget {
+  const AccountProfile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(backgroundImage: AssetImage('assets/images/profil.jpg'),),
+      title: Text(
+        'George Lee',
+        style: TextStyle(
+            fontSize: Constant.fontTitleProfile, fontWeight: FontWeight.w500),
+      ),
+      subtitle: Text('Lee.George@mail.com',
+          style: TextStyle(fontSize: Constant.fontSemiBig)),
+    );
+  }
+}
