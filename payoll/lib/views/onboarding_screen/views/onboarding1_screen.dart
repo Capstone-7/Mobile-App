6f3b06ff1b5497310670cac9631803498e318ba5
Madd-G <akhmadnuralamsyah@gmail.com>
2022-12-02 21:26:00 +0700

    modified onboarding screen

--- payoll/lib/views/onboarding_screen/views/onboarding1_screen.dart
+++ payoll/lib/views/onboarding_screen/views/onboarding1_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:payoll/utils/constant.dart';
-import 'package:payoll/views/login_screen/views/login_screen.dart';
-import '../../../widgets/bottom_nav_bar.dart';
-import '../widgets/onboarding_button.dart';
+import 'package:payoll/views/onboarding_screen/widgets/lanjutkan_button.dart';
+import 'package:payoll/views/onboarding_screen/widgets/lewati_button.dart';
 import '../widgets/pointer_bar.dart';
 import 'onboarding2_screen.dart';
 
@@ -19,100 +18,85 @@ class Onboarding1 extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(24.0),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              SizedBox(
-                height: size.height * 0.028,
-              ),
-              SizedBox(
-                  height: size.height * 0.4,
-                  width: size.width * 0.8,
-                  child: Image.asset(
-                    'assets/images/onboarding/onboarding-1.png',
-                  )),
-              SizedBox(
-                height: size.height * 0.03,
-              ),
-              const Text(
-                'Easy Peasy',
-                style: TextStyle(
-                    fontSize: Constant.fontExtraBig,
-                    fontWeight: FontWeight.w700),
-              ),
-              SizedBox(
-                height: size.height * 0.02,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 4.0),
-                child: SizedBox(
-                  height: size.height * 0.07,
-                  child: const Text(
-                    'Don’t worry be happy, Transaksi pulsa, paket data, bayar tagihan, dan lainnya jadi gampang pake banget',
-                    textAlign: TextAlign.center,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                      height: size.height * 0.4,
+                      width: size.width * 0.8,
+                      child: Image.asset(
+                        'assets/images/onboarding/onboarding-1.png',
+                      )),
+                  SizedBox(
+                    height: size.height * 0.02,
+                  ),
+                  const Text(
+                    'Easy Peasy',
                     style: TextStyle(
-                      fontSize: 17.0,
+                        fontSize: Constant.fontExtraBig,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  SizedBox(
+                    height: size.height * 0.02,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                    child: SizedBox(
+                      height: size.height * 0.07,
+                      child: const Text(
+                        'Don’t worry be happy, Transaksi pulsa, paket data, bayar tagihan, dan lainnya jadi gampang pake banget',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 17.0,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              SizedBox(height: size.height * 0.13),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  PointerBar(size: size, active: true),
-                  SizedBox(
-                    width: size.width * 0.03,
-                  ),
-                  PointerBar(size: size, active: false),
-                  SizedBox(
-                    width: size.width * 0.03,
-                  ),
-                  PointerBar(size: size, active: false),
-                  SizedBox(
-                    width: size.width * 0.03,
-                  ),
-                  PointerBar(size: size, active: false),
-                  SizedBox(
-                    width: size.width * 0.03,
-                  ),
-                  PointerBar(size: size, active: false),
                 ],
               ),
-              SizedBox(
-                height: size.height * 0.03,
-              ),
-              OnboardingButton(
-                onPressed: () {
-                  Navigator.pushNamed(
-                    context,
-                    Onboarding2.routeName,
-                  );
-                },
-              ),
-              SizedBox(
-                height: size.height * 0.03,
-              ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.pushReplacementNamed(context, LoginScreen.routeName);
-                  // Navigator.pushAndRemoveUntil(
-                  //     context,
-                  //     MaterialPageRoute(
-                  //         builder: (context) => const BottomNavBar(
-                  //           pageIndex: 0,
-                  //         )),
-                  //         (route) => false);
-                },
-                child: const Text(
-                  'Lewati',
-                  style: TextStyle(
-                      color: Color(
-                        0xff396EB0,
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      PointerBar(size: size, active: true),
+                      SizedBox(
+                        width: size.width * 0.03,
                       ),
-                      fontSize: Constant.fontSemiBig,
-                      fontWeight: FontWeight.w500),
-                ),
-              )
+                      PointerBar(size: size, active: false),
+                      SizedBox(
+                        width: size.width * 0.03,
+                      ),
+                      PointerBar(size: size, active: false),
+                      SizedBox(
+                        width: size.width * 0.03,
+                      ),
+                      PointerBar(size: size, active: false),
+                      SizedBox(
+                        width: size.width * 0.03,
+                      ),
+                      PointerBar(size: size, active: false),
+                    ],
+                  ),
+                  SizedBox(
+                    height: size.height * 0.03,
+                  ),
+                  LanjutkanButton(
+                    onPressed: () {
+                      Navigator.pushNamed(
+                        context,
+                        Onboarding2.routeName,
+                      );
+                    },
+                  ),
+                  SizedBox(
+                    height: size.height * 0.02,
+                  ),
+                  const LewatiButton()
+                ],
+              ),
             ],
           ),
         ),
--- payoll/lib/views/onboarding_screen/views/onboarding4_screen.dart
+++ payoll/lib/views/onboarding_screen/views/onboarding4_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:payoll/utils/constant.dart';
-import '../../../widgets/bottom_nav_bar.dart';
-import '../../login_screen/views/login_screen.dart';
-import '../widgets/onboarding_button.dart';
+import 'package:payoll/views/onboarding_screen/widgets/lewati_button.dart';
+import '../widgets/lanjutkan_button.dart';
 import '../widgets/pointer_bar.dart';
 import 'onboarding5_screen.dart';
 
@@ -14,103 +13,90 @@ class Onboarding4 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-
     return Scaffold(
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(24.0),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              SizedBox(
-                height: size.height * 0.028,
-              ),
-              SizedBox(
-                  height: size.height * 0.4,
-                  width: size.width * 0.8,
-                  child: Image.asset(
-                    'assets/images/onboarding/onboarding-4.png',
-                  )),
-              SizedBox(
-                height: size.height * 0.03,
-              ),
-              const Text(
-                'Anti Admin Club',
-                style: TextStyle(
-                    fontSize: Constant.fontExtraBig,
-                    fontWeight: FontWeight.w700),
-              ),
-              SizedBox(
-                height: size.height * 0.02,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 4.0),
-                child: SizedBox(
-                  height: size.height * 0.07,
-                  child: const Text(
-                    'Gak ada biaya tambahan buat kamu yang lagi bayar tagihan.',
-                    textAlign: TextAlign.center,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                      height: size.height * 0.4,
+                      width: size.width * 0.8,
+                      child: Image.asset(
+                        'assets/images/onboarding/onboarding-4.png',
+                      )),
+                  SizedBox(
+                    height: size.height * 0.02,
+                  ),
+                  const Text(
+                    'Anti Admin Club',
                     style: TextStyle(
-                      fontSize: 17.0,
+                        fontSize: Constant.fontExtraBig,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  SizedBox(
+                    height: size.height * 0.02,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                    child: SizedBox(
+                      height: size.height * 0.07,
+                      child: const Text(
+                        'Gak ada biaya tambahan buat kamu yang lagi bayar tagihan.',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 17.0,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              SizedBox(height: size.height * 0.13),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  PointerBar(size: size, active: false),
-                  SizedBox(
-                    width: size.width * 0.03,
-                  ),
-                  PointerBar(size: size, active: false),
-                  SizedBox(
-                    width: size.width * 0.03,
-                  ),
-                  PointerBar(size: size, active: false),
-                  SizedBox(
-                    width: size.width * 0.03,
-                  ),
-                  PointerBar(size: size, active: true),
-                  SizedBox(
-                    width: size.width * 0.03,
-                  ),
-                  PointerBar(size: size, active: false),
                 ],
               ),
-              SizedBox(
-                height: size.height * 0.03,
-              ),
-              OnboardingButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, Onboarding5.routeName);
-                },
-              ),
-              SizedBox(
-                height: size.height * 0.03,
-              ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.pushReplacementNamed(context, LoginScreen.routeName);
-                  // Navigator.pushAndRemoveUntil(
-                  //     context,
-                  //     MaterialPageRoute(
-                  //         builder: (context) => const BottomNavBar(
-                  //               pageIndex: 0,
-                  //             )),
-                  //     (route) => false);
-                },
-                child: const Text(
-                  'Lewati',
-                  style: TextStyle(
-                      color: Color(
-                        0xff396EB0,
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      PointerBar(size: size, active: false),
+                      SizedBox(
+                        width: size.width * 0.03,
                       ),
-                      fontSize: Constant.fontSemiBig,
-                      fontWeight: FontWeight.w500),
-                ),
-              )
+                      PointerBar(size: size, active: false),
+                      SizedBox(
+                        width: size.width * 0.03,
+                      ),
+                      PointerBar(size: size, active: false),
+                      SizedBox(
+                        width: size.width * 0.03,
+                      ),
+                      PointerBar(size: size, active: true),
+                      SizedBox(
+                        width: size.width * 0.03,
+                      ),
+                      PointerBar(size: size, active: false),
+                    ],
+                  ),
+                  SizedBox(
+                    height: size.height * 0.03,
+                  ),
+                  LanjutkanButton(
+                    onPressed: () {
+                      Navigator.pushNamed(
+                        context,
+                        Onboarding5.routeName,
+                      );
+                    },
+                  ),
+                  SizedBox(
+                    height: size.height * 0.02,
+                  ),
+                  const LewatiButton()
+                ],
+              ),
             ],
           ),
         ),
