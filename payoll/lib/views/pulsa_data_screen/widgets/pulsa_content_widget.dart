c5f7145f20515b422461587a9a4d602ab42aa68c
alamsyah <akhmadnuralamsyah@gmail.com>
2022-12-15 14:24:28 +0700

    Feature/alam implementing api for product (#24)
    * implemented API to get products
    
    * implemented product API before payment

--- payoll/lib/views/pulsa_data_screen/widgets/pulsa_content_widget.dart
+++ payoll/lib/views/pulsa_data_screen/widgets/pulsa_content_widget.dart
@@ -5,6 +5,8 @@ import 'package:payoll/utils/constant.dart';
 import 'package:payoll/utils/state/finite_state.dart';
 import 'package:provider/provider.dart';
 
+import '../../payment_screen/views/payment_screen.dart';
+
 class PulsaContentWidget extends StatelessWidget {
   const PulsaContentWidget({
     Key? key,
@@ -13,11 +15,6 @@ class PulsaContentWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-    final oCcy = NumberFormat.currency(
-        locale: 'id',
-        customPattern: '\u00a4 #,### ',
-        decimalDigits: 0,
-        symbol: 'Rp');
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Consumer<ProductProvider>(builder: (context, provider, _) {
@@ -39,57 +36,171 @@ class PulsaContentWidget extends StatelessWidget {
                       crossAxisCount: 2, mainAxisExtent: size.height * 0.16),
                   itemBuilder: (BuildContext context, int index) {
                     final product = provider.productModel!.data![index];
-
                     return Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        decoration: const BoxDecoration(
-                            color: Colors.white,
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(8.0)),
-                            boxShadow: [
-                              BoxShadow(
-                                  color: Color(0x29292914),
-                                  blurRadius: 10.0,
-                                  blurStyle: BlurStyle.normal,
-                                  offset: Offset(0.0,
-                                      0.75) // shadow direction: bottom right
-                                  )
-                            ]),
-                        child: Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                '${product.description} Rp${product.price}',
-                                style: const TextStyle(
-                                    fontSize: Constant.fontRegular,
-                                    fontWeight: FontWeight.w500),
-                              ),
-                              SizedBox(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
+                      child: GestureDetector(
+                        onTap: () {
+                          showModalBottomSheet(
+                              isScrollControlled: true,
+                              context: context,
+                              builder: (BuildContext context) {
+                                return Wrap(
                                   children: [
-                                    const Text(
-                                      'Harga',
-                                      style: TextStyle(
-                                          fontSize: Constant.fontSemiSmall),
-                                    ),
-                                    Text(
-                                      oCcy.format(
-                                        product.price,
+                                    Padding(
+                                      padding: const EdgeInsets.fromLTRB(
+                                          18.0, 10.0, 18.0, 20.0),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Center(
+                                            child: Container(
+                                              width: 90.0,
+                                              height: 6.0,
+                                              decoration: const BoxDecoration(
+                                                  color: Color(0xFFC5C5C5),
+                                                  borderRadius:
+                                                      BorderRadius.all(
+                                                          Radius.circular(
+                                                              10.0))),
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            height: size.height * 0.02,
+                                          ),
+                                          Text(
+                                            '${product.description}',
+                                            style: const TextStyle(
+                                                fontSize: Constant.fontBig,
+                                                fontWeight: FontWeight.w700),
+                                          ),
+                                          SizedBox(
+                                            height: size.height * 0.015,
+                                          ),
+                                          Text(
+                                            '${product.details}',
+                                            style: const TextStyle(
+                                              fontSize: Constant.fontRegular,
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            height: size.height * 0.03,
+                                          ),
+                                          Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              const Text(
+                                                'Harga',
+                                                style: TextStyle(
+                                                    fontSize: 20,
+                                                    fontWeight:
+                                                        FontWeight.w500),
+                                              ),
+                                              Text(
+                                                Constant.oCcy.format(
+                                                  product.price,
+                                                ),
+                                                style: const TextStyle(
+                                                    fontSize: 20,
+                                                    fontWeight: FontWeight.w500,
+                                                    color: Color(0xFF396EB0)),
+                                              ),
+                                            ],
+                                          ),
+                                          SizedBox(
+                                            height: size.height * 0.03,
+                                          ),
+                                          GestureDetector(
+                                            onTap: () {
+                                              Navigator.push(
+                                                  context,
+                                                  MaterialPageRoute(
+                                                      builder: (context) =>
+                                                          PaymentScreen(
+                                                            index: index,
+                                                          )));
+                                            },
+                                            child: Container(
+                                              decoration: const BoxDecoration(
+                                                color: Color(0xFF396EB0),
+                                                borderRadius: BorderRadius.all(
+                                                  Radius.circular(10.0),
+                                                ),
+                                              ),
+                                              child: const Center(
+                                                child: Padding(
+                                                  padding: EdgeInsets.all(16.0),
+                                                  child: Text(
+                                                    'LANJUT KE PEMBAYARAN',
+                                                    style: TextStyle(
+                                                        color: Colors.white,
+                                                        fontSize: Constant
+                                                            .fontSemiBig,
+                                                        fontWeight:
+                                                            FontWeight.w500),
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                          )
+                                        ],
                                       ),
-                                      style: const TextStyle(
-                                          fontSize: Constant.fontSemiBig,
-                                          fontWeight: FontWeight.w700,
-                                          color: Color(0xFF396EB0)),
                                     ),
                                   ],
+                                );
+                              });
+                        },
+                        child: Container(
+                          decoration: const BoxDecoration(
+                              color: Colors.white,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(8.0)),
+                              boxShadow: [
+                                BoxShadow(
+                                    color: Color(0x29292914),
+                                    blurRadius: 10.0,
+                                    blurStyle: BlurStyle.normal,
+                                    offset: Offset(0.0,
+                                        0.75) // shadow direction: bottom right
+                                    )
+                              ]),
+                          child: Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  '${product.description}',
+                                  style: const TextStyle(
+                                      fontSize: Constant.fontRegular,
+                                      fontWeight: FontWeight.w500),
                                 ),
-                              )
-                            ],
+                                SizedBox(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      const Text(
+                                        'Harga',
+                                        style: TextStyle(
+                                            fontSize: Constant.fontSemiSmall),
+                                      ),
+                                      Text(
+                                        Constant.oCcy.format(
+                                          product.price,
+                                        ),
+                                        style: const TextStyle(
+                                            fontSize: Constant.fontSemiBig,
+                                            fontWeight: FontWeight.w700,
+                                            color: Color(0xFF396EB0)),
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
                           ),
                         ),
                       ),
